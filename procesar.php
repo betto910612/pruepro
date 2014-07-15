bad1caa8dde635461e8eb7f0e35ca03cb532d169
betto <betto2211@gmail.com>
2014-07-14 20:42:01 -0500

    mn
    b

--- procesar.php
+++ procesar.php
@@ -7,8 +7,9 @@ $telefono=$_POST["telefono"];
 $correo=$_POST["correo"];
 $latitud=$_POST["latitud"];
 $longitud=$_POST["longitud"];
+$direccion=$_POST["direccion"];
 
-$sql="INSERT INTO servicios(id, nombre, direccion, telefono, correo, latitud, longitud)VALUES('','$nombre','$direccion','$telefono','$correo','$latitud','$longitud')";
+$sql="INSERT INTO servicios(id, nombre, direccion, telefono, correo, latitud,longitud,direccion)VALUES('','$nombre','$direccion','$telefono','$correo','$latitud','$longitud','$direccion')";
 $resultado=mysql_query($sql,$con);
 
 if($resultado){
