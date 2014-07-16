981f57eb723feca7b73eb5552536946786d205c4
betto <betto2211@gmail.com>
2014-07-16 11:28:44 -0500

    tr
    tr

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ window.onload=function(){
 };
 
 function capturePhoto() {
-    navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 90,
+    navigator.camera.getPicture(onPhotoURISuccess, uploadPhoto, onFail, { quality: 90,
         <!--destinationType: Camera.DestinationType.DATA_URL,
 		destinationType: Camera.DestinationType.FILE_URI,
 		correctOrientation: true,
@@ -33,10 +33,40 @@ function onSuccess(imageData) {
     document.getElementById("foto").style.backgroundSize="100% 100%";
 }
 
+
+
 function onPhotoURISuccess(imageURI) {
     document.getElementById("foto").style.backgroundImage="url('"+imageURI+"')";
     document.getElementById("foto").style.backgroundSize="100% 100%";
 }
+
+
+function uploadPhoto(imageURI) {
+            var options = new FileUploadOptions();
+            options.fileKey="file";
+            options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
+            options.mimeType="image/jpeg";
+
+            var params = {};
+            params.value1 = "test";
+            params.value2 = "param";
+
+            options.params = params;
+
+            var ft = new FileTransfer();
+            ft.upload(imageURI, encodeURI("http://parkingapp.260mb.net/subir.php"), win, fail, options);
+        }
+		
+		
+		function win(r) {
+            alert("Se ah guardado la imagen");
+        }
+
+        function fail(error) {
+            alert("Ah Ocurrido un error: Codigo = " + error.code);
+            
+        } 
+
 
 function onFail(message) {
     alert('Error por: ' + message);
