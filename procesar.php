e7cfd4c0ee91c50298e5b8b0d42727d6a37f9854
betto <betto2211@gmail.com>
2014-07-17 11:23:19 -0500

    wq
    qw

--- procesar.php
+++ procesar.php
@@ -1,6 +1,40 @@
 <?php
 require 'conexion.php';
 
+function specialCharacters($s) {
+
+$s = str_replace("[áàâãªä@]","a",$s);
+
+$s = str_replace("[ÁÀÂÃÄ]","A",$s);
+
+$s = str_replace("[éèêë]","e",$s);
+
+$s = str_replace("[ÉÈÊË]","E",$s);
+
+$s = str_replace("[íìîï]","i",$s);
+
+$s = str_replace("[ÍÌÎÏ]","I",$s);
+
+$s = str_replace("[óòôõºö]","o",$s);
+
+$s = str_replace("[ÓÒÔÕÖ]","O",$s);
+
+$s = str_replace("[úùûü]","u",$s);
+
+$s = str_replace("[ÚÙÛÜ]","U",$s);
+
+$s = str_replace("[¿?]","_",$s);
+
+$s = str_replace("ñ","n",$s);
+
+$s = str_replace("Ñ","N",$s);
+
+ 
+
+return $s;
+}
+
+
 $nombre=$_POST["nombre"];
 $direccion=$_POST["direccion"];
 $telefono=$_POST["telefono"];
