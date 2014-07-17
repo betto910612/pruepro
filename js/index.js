3306a8e99e3b984237ff14d3a323b5b34ca873f8
betto <betto2211@gmail.com>
2014-07-17 11:53:44 -0500

    qwe
    qew

--- js/index.js
+++ js/index.js
@@ -21,16 +21,49 @@ window.onload=function(){
 
 
 function capturePhoto() {
-      navigator.camera.getPicture(onSuccess, onFail, { quality: 90,
+      navigator.camera.getPicture(EnviarServidor, onFail, { quality: 90,
         destinationType: destinationType.DATA_URL,
 		sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
 		 });
     }
 
 
+function EnviarServidor(image){
+                          
+
+document.getElementById("foto").style.backgroundImage="url('data:image/jpeg;base64,"+imageData+"')";
+ document.getElementById("foto").style.backgroundSize="100% 100%";
+                         
+path = image.fullPath;
+name = image.name;
+                                
+var options = new FileUploadOptions();
+options.fileKey="file";
+options.fileName=image.name;
+options.mimeType="image/jpeg";
+                               
+var params = new Object();
+params.fullpath = path;
+params.name = name;
+
+options.params = params;
+options.chunkedMode = false;
+var url="http://parkingapp.260mb.net/subir.php";
+var url2="http://parkingapp.260mb.net/subir.php";
+var ft = new FileTransfer();
+ft.upload(image, url, win, fail, options, true);
+                            }
+function win(r) {
+ alert("Imagen cargada al servidor!!");
+                          }
+                          //Failure callback
+function fail(error) {
+alert("Ocurrio un error cargando la imagen");
+                          }
+
 function onSuccess(imageData) {
-    document.getElementById("foto").style.backgroundImage="url('data:image/jpeg;base64,"+imageData+"')";
-    document.getElementById("foto").style.backgroundSize="100% 100%";
+document.getElementById("foto").style.backgroundImage="url('data:image/jpeg;base64,"+imageData+"')";
+ document.getElementById("foto").style.backgroundSize="100% 100%";
 }
 
 
